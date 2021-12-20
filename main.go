e5da6e43a2f22eeae87130c340456a32372f2b97
Peter Etherington <Peter.Etherington@skybettingandgaming.com>
2021-12-20 09:39:39 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"constraints"
+	"fmt"
+)
+
+type Number interface {
+	int64 | float64
+}
+
+func main() {
+	// Initialize a map for the integer values
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	// Initialize a map for the float values
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
+
+	a := 4
+	b := 2
+	fmt.Printf("Non-Generic Min of %d and %d: %d\n", a, b, min(a, b))
+
+	fmt.Printf("Generic Min of %d and %d: %d\n", a, b, minGeneric(a, b))
+}
+
+// SumInts adds together the values of m.
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumFloats adds together the values of m.
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumIntsOrFloats sums the values of map m. It supports both int64 and float64
+// as types for map values.
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumNumbers sums the values of map m. Its supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+func minGeneric[T constraints.Ordered](a, b T) T {
+	if a < b {
+		return a
+	}
+
+	return b
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Generics
+
+Messing with Generics in go1.18beta1, following guides:
+
+https://go.dev/blog/go1.18beta1
+
+https://go.dev/doc/tutorial/generics
+
+https://www.youtube.com/watch?v=35eIxI_n5ZM&t=1755s
+
+## Install Go1.18 Beta 1
+
+```
+go get golang.org/dl/go1.18beta1@latest
+go1.18beta1 download
+```
+
+## Run Generics Program
+
+`go1.18beta1 run ./...`
